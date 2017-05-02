fd1070d70f1b99ee91df1158f9eecde5d0015338
Matthew Biesen <upandcomming88@gmail.com>
2017-05-02 03:29:41 -0400

    Create python_pointer.py

---
+++ python_pointer.py
@@ -0,0 +1,35 @@
+# Script to receive data from the microbit and use that data to move our laser pointer
+
+import RPi.GPIO as GPIO
+import serial
+
+# Set the variables that concern our serial communication 
+PORT = "/dev/ttyACM0"
+BAUD = 115200
+s = serial.Serial(PORT)
+s.baudrate = BAUD
+s.parity = serial.PARITY_NONE
+s.databits = serial.EIGHTBITS
+s.stopbits = serial.STOPBITS_ONE
+
+# Set the variables that control our servos
+servo1 = 13 #placeholder
+servo2 = 16 #placeholder
+
+GPIO.setmode(GPIO.BOARD) #declare the reference style for GPIO
+GPIO.setup(servo1,GPIO.OUT) #assign pin as an output
+pwm1=GPIO.PWM(servo1,50) #sets PWM for the pin
+GPIO.setup(servo2,GPIO.OUT) #assign pin as an output
+pwm2=GPIO.PWM(servo2,50) #sets PWM for the pin
+
+
+#####pwm.start(5)
+#####pwm.ChangeDutyCycle(spam) #moves pointer to location designated by spam
+
+def main():
+  #determine microbit starting position
+  
+  while True:
+    data = s.readline().decode('UTF-8')   #check for data.  this code blocks script from moving forward until data is received.
+    datalist = data.rstrip().split(' ')
+    
