46acba5b22468af7fd52ab07eeb911880a8f82f2
Matthew Biesen <upandcomming88@gmail.com>
2017-05-02 22:32:35 -0500

    Update microbit_controller.py

--- microbit_controller.py
+++ microbit_controller.py
@@ -1,1 +1,11 @@
 # code to be placed on the battery powered microbit controller
+from microbit import *
+import radio
+
+radio.on()
+
+while True:
+  my_send = accelerometer.get_values()
+  display.scroll(my_send[0])
+  radio.send(my_send)
+  
