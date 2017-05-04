277a54c893dfb3421049013f464929e3617e52da
Matthew Biesen <upandcomming88@gmail.com>
2017-05-03 20:09:45 -0400

    Update microbit_controller.py

--- microbit_controller.py
+++ microbit_controller.py
@@ -2,15 +2,19 @@
 from microbit import *
 import radio
 
-my_switch = True
+my_switch = False
 
 radio.on()
 
+# Send accelerometer data to the hub microbit
 def send_acc_data():
     my_send = accelerometer.get_values()                # Get acc data
     radio.send(str(my_send[0])+"," + str(my_send[1]))   # Send only x and y data as comma separated string
     sleep(800)    
 
+# Loop forever, check for button presses, send data accordingly
+# A button will trigger a single data send, b button sets switch and activates a new loop to consistently send data
+# The switch variable controls the b button while loop.  When true loop is executed, if false its not
 while True:
     if button_a.was_pressed():
         send_acc_data()
